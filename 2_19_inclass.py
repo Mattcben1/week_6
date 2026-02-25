9f169c52ae6b87faaf47702f6af927a6cdf96b60
Mattcben1 <bxp6yd@virginia.edu>
2026-02-25 05:23:25 +0000

    Finished for the night

---
+++ 2_19_inclass.py
@@ -0,0 +1,44 @@
+#%%
+# import Libraries
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+#%%
+# load data
+#help(pd.read_csv)
+df = pd.read_csv("house_votes_Dem.csv", encoding='latin-1')
+# %%
+# take a look at the data
+df.info()
+# %%
+# separate out the numeric features
+c_num = df[["aye", "nay", "other"]]
+# %%
+# documentation for kmeans in sklearn
+
+# %% build a kmeans model
+kmeans = KMeans(n_clusters=3, random_state=42, verbose=1)
+kmeans.fit(c_num)
+
+# %% look at the information in the model
+print(kmeans.cluster_centers_)
+print(kmeans.labels_)
+# %%
+# add the cluster labels to the original data frame
+df['cluster'] = kmeans.labels_ 
+# %%
+# for loop to check differrent clusters
+inertias = []
+k_values = range(1, 10)
+for k in k_values:
+    kmeans = KMeans(n_clusters=k, random_state=42, verbose=0)
+    kmeans.fit(c_num)
+    inertias.append(kmeans.inertia_)
+# %% simple plot of the clusters
+plt.figure(figsize=(10,5))
+plot.plot(k_values, inertias, marker='o')
+plt.xlabel('number of clusters(k)')
+plt.ylabel('inertia')
+ 
+# %%
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # week_6
+
+## Virtual environment
+
+Steps to create and use a local virtual environment for this project:
+
+- Create the virtual environment and install dependencies:
+
+```bash
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip setuptools wheel
+pip install -r requirements.txt
+```
+
+- Or run the helper script:
+
+```bash
+./venv_setup.sh
+```
+
+After activation use `deactivate` to exit the virtual environment.
